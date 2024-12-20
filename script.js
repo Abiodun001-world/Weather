8a02e60aa346b8f8ac6bcdc0346ad3d46331fe4c
Abiodun Adekunle <94719889+Abiodun001-world@users.noreply.github.com>
2024-12-20 20:52:41 +0000

    resovle merge conflicts

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 // fetching and displaying the weather in a Country 
 let weather = {
+<<<<<<< HEAD
         "apikey": "85003a0bbdc3a50e6ee03456c2fdc06f",
       fetchWeather: function(city) {
         fetch(
         "https://api.openweathermap.org/data/2.5/weather?q="
+=======
+    "apikey": "85003a0bbdc3a50e6ee03456c2fdc06f",
+    fetchWeather: function(city) {
+        fetch(
+           "https://api.openweathermap.org/data/2.5/weather?q="
+>>>>>>> parent of 135a15d (testing google apis)
             + city
             + "&units=metric&appid="
             + this.apikey
@@ -21,6 +28,7 @@ let weather = {
         });
     },
     displayWeather: function(data) {
+<<<<<<< HEAD
         const { name } = data;
         const { icon, description } = data.weather[0];
         const { temp, humidity } = data.main;
@@ -45,6 +53,24 @@ let weather = {
     },
     search: function() {
         this.fetchWeather(document.querySelector(".search-bar").value);
+=======
+     const { name } = data;
+     const { icon, description } = data.weather[0];
+      const { temp, humidity} = data.main;
+      const { speed } = data.wind;
+      document.querySelector(".city").innerText = "Weather in " + name;
+      document.querySelector(".icon").src = "https://openweathermap.org/img/wn/"+ icon + ".png";
+      document.querySelector (".temp").innerText = temp + "°C";
+      document.querySelector(".humidity").innerHTML ="Humidity: " + humidity + "%";
+      document.querySelector(".wind").innerHTML ="Wind speed : " + speed + " km/h";
+      document.querySelector(".weather").classList.remove("loading");
+      document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')";
+  
+},
+
+    search: function(){
+      this.fetchWeather(document.querySelector(".search-bar").value);
+>>>>>>> parent of 135a15d (testing google apis)
     }
 };
 
