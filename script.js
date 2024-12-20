0884bb652e98a3d8116d7c1eee60917e3fc8caed
Abiodun Adekunle <94719889+Abiodun001-world@users.noreply.github.com>
2024-12-20 21:03:50 +0100

    testing google apis key

--- script.js
+++ script.js
@@ -1,89 +1,52 @@
 // fetching and displaying the weather in a Country 
 let weather = {
-    //85003a0bbdc3a50e6ee03456c2fdc06f
+    // Replace with your actual Google API key
     "apikey": "AIzaSyBUGzHOJ0Eohu3MKqzbeWZ032L0ynt41uI",
     fetchWeather: function(city) {
         fetch(
-            // https://api.openweathermap.org/data/2.5/weather?q=
-           "https://airquality.googleapis.com/v1/currentConditions:lookup?"
+            "https://maps.googleapis.com/maps/api/weather?q="
             + city
-             + "&units=metric&appid="
-             + this.apikey
+            + "&units=metric&key="
+            + this.apikey
         )
-      .then((response) => {
-        if (!response.ok) {
-          alert("No weather found.");
-          throw new Error("No weather found.");
-        }
-        return response.json();
-      })
-      .then((data) => this.displayWeather(data));
-        /*this code below is for searching(a thing that is different from weather) 
-         weather in a country that does not exit which is not going to throw an error.
-        .then((response) => response.json())
-        .then((data) => this.displayWeather(data));*/
+        .then((response) => {
+            if (!response.ok) {
+                alert("No weather found.");
+                throw new Error("No weather found.");
+            }
+            return response.json();
+        })
+        .then((data) => this.displayWeather(data))
+        .catch((error) => {
+            console.error("Error fetching weather data:", error);
+        });
     },
     displayWeather: function(data) {
-     const { name } = data;
-     const { icon, description } = data.weather[0];
-      const { temp, humidity} = data.main;
-      const { speed } = data.wind;
-      document.querySelector(".city").innerText = "Weather in " + name;
-        // https://openweathermap.org/img/wn/
-      document.querySelector(".icon").src = ""+ icon + ".png";
-      document.querySelector (".temp").innerText = temp + "°C";
-      document.querySelector(".humidity").innerHTML ="Humidity: " + humidity + "%";
-      document.querySelector(".wind").innerHTML ="Wind speed : " + speed + " km/h";
-      document.querySelector(".weather").classList.remove("loading");
-      document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')";
-  
-},
-
-    search: function(){
-      this.fetchWeather(document.querySelector(".search-bar").value);
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind speed: " + speed + " km/h";
+        document.querySelector(".weather").classList.remove("loading");
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')";
+    },
+    search: function() {
+        this.fetchWeather(document.querySelector(".search-bar").value);
     }
 };
 
-
-document
-.querySelector(".search button")
-.addEventListener("click",function(){
-  weather.search();
-
+document.querySelector(".search button").addEventListener("click", function() {
+    weather.search();
 });
 
-document.querySelector(".search-bar").addEventListener("keyup", function(event){
- if(event.key == "Enter") {
-    weather.search();
- }
-})
+document.querySelector(".search-bar").addEventListener("keyup", function(event) {
+    if (event.key == "Enter") {
+        weather.search();
+    }
+});
 
-
-weather.fetchWeather("Nigeria"); 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+weather.fetchWeather("Nigeria");
